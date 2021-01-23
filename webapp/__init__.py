83d97130ee174fa7752cb38bb9285f9de3d82a22
Roman Antonyuk <roman2062010@yandex.ru>
2021-01-23 22:26:57 +0500

    Добавлена кнопка 'Местоположение', отображающая станции метро 23.01.2021

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from webapp.forms import LoginForm
 from webapp.model import db, Ad, Location, User
 
 
@@ -11,27 +12,36 @@ def create_app():
     @app.route("/")
     def index():
         products = Ad.query.all()
-        return render_template("products_list.html", products=products)
-    
+        users = User.query.all()
+        locations = Location.query.all()
+        return render_template("products_list.html", products=products, users=users, locations=locations)
 
+    
+    @app.route('/login')
+    def login():
+        title = "Авторизация" 
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+
+    
     @app.route("/product/<product_id>/")
     def product(product_id):
         product = Ad.query.filter(Ad.id == product_id).first()
         return render_template("product.html", product=product)
 
 
-    @app.route("/<region_name>/")
-    def region(region_name):
-        print(region_name)
-        products = Ad.query.join(Location,Ad.location_id == Location.id).filter(Location.name == region_name).all()
-        return render_template("products_list.html", products=products)
+    @app.route("/locations/<location_id>/")
+    def regions(location_id):
+        products = Ad.query.join(Location,Ad.location_id == Location.id).filter(Location.id == location_id).all()
+        locations = Location.query.all()
+        return render_template("products_list.html", products=products, locations=locations)
 
 
-    @app.route("/<user_name>/")
-    def user(user_name):
-        print(user_name)
-        products = Ad.query.join(User,Ad.user_id == User.id).filter(User.username == user_name).all()
-        return render_template("products_list.html", products=products)
-
+    @app.route("/users/<user_id>/")
+    def users(user_id):
+        products = Ad.query.join(User,Ad.user_id == User.id).filter(User.id == user_id).all()
+        users = User.query.all()
+        return render_template("products_list.html", products=products, users=users)
+ 
 
     return app
